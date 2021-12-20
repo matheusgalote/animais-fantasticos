5c449b5d1b3b33e1b937423a041c76d70191902f
Trauvin <mateusgalote@gmail.com>
2021-12-20 18:25:49 -0300

    refatoracao com classe

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -1,11 +1,15 @@
-export default function initAnimacaoScroll() {
-  const sections = document.querySelectorAll('[data-anime="scroll"]');
-  const windowMetade = window.innerHeight * 0.6;
+export default class ScrollAnima {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowMetade = window.innerHeight * 0.6;
 
-  function animaScroll() {
-    sections.forEach((section) => {
+    this.animaScroll = this.animaScroll.bind(this);
+  }
+
+  animaScroll() {
+    this.sections.forEach((section) => {
       const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = (sectionTop - windowMetade) < 0;
+      const isSectionVisible = (sectionTop - this.windowMetade) < 0;
       if (isSectionVisible) {
         section.classList.add('ativo');
       } else if (section.classList.contains('ativo')) {
@@ -13,8 +17,9 @@ export default function initAnimacaoScroll() {
       }
     });
   }
-  if (sections.length) {
-    animaScroll();
-    window.addEventListener('scroll', animaScroll);
+
+  init() {
+    window.addEventListener('scroll', this.animaScroll);
+
   }
 }
